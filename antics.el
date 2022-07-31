024b9705d4ad2ec2850dd91c6aa869184137b35d
John Nelson <jnelson@johncoder.com>
2022-07-31 07:34:19 -0400

    progress!

--- antics.el
+++ antics.el
@@ -21,11 +21,14 @@
 (require 'cl-lib)
 (require 'eieio)
 
+(defvar antics-filename "example.antics"
+  "The file to be loaded by antics.")
+
 (defun antics--read-config (antics-file-path)
   "Read s-expression from ANTICS-FILE-PATH."
-  (with-current-buffer (find-file-noselect antics-file-path)
-                 (goto-char (point-min))
-                 (read (current-buffer))))
+  (cadr (with-current-buffer (find-file-noselect antics-file-path)
+          (goto-char (point-min))
+          (read (current-buffer)))))
 
 (defclass antics--configuration ()
   ((name :initarg :name
@@ -35,7 +38,42 @@
 
 (defclass antics--item ()
   ((name :initarg :name
-         :initform "Unknown Item")))
+         :initform "Unknown Item")
+   (cwd :initarg :cwd
+        :initform "./")
+   (cmd :initarg :cmd
+        :initform '(error "Property CMD is required for antics items"))
+   (proc :initform nil)))
+
+(cl-defmethod procname ((obj antics--item))
+  "Process name for ANTICS--ITEM instance OBJ."
+  (with-slots (name) obj
+    (format "antics: %s" name)))
+
+(cl-defmethod start ((obj antics--item))
+  "Start a process for ANTICS--ITEM instance OBJ."
+  (with-slots (proc name cwd cmd) obj
+    (unless proc
+      (setq proc (make-process :name (procname obj)
+                               :buffer (procname obj)
+                               :command (list "bash")
+                               :connection-type 'pipe))
+      (process-send-string proc cmd)
+      (message "starting process for %s" name))))
+
+(defun antics--parse-item (lst)
+  "Create an antics item from LST."
+  (let (name cwd cmd current)
+    (setq name (pop lst))
+    (while lst
+      (setq current (pop lst))
+      (when (symbolp current)
+        (cond ((eq current :cwd)
+               (setq cwd (pop lst)))
+              ((eq current :cmd)
+               (setq cmd (pop lst)))
+              (t (message "unknown symbol in antics item")))))
+    (antics--item :name name :cwd cwd :cmd cmd)))
 
 (defun antics--parse-config (config)
   "Parse s-expressions in CONFIG."
@@ -47,17 +85,55 @@
                (setq name (pop config)))
               ((eq current :items)
                (while (and config (listp (car config)))
-                 (push (pop config) items)))
+                 (push (antics--parse-item (pop config)) items)))
               (t (message "unknown symbol %s" current)))))
     (antics--configuration :name name :items items)))
 
-(let ((config (cadr (antics--read-config "example.antics"))))
-  (antics--parse-config config))
+(defun antics--mode-cols ()
+  "Columns for antics-mode."
+  (vector (list "Name" (/ 100 3))
+          (list "CWD" (/ 100 3))
+          (list "Command" (/ 100 3))))
+
+(defun antics--mode-rows (config)
+  "Rows for anticss-mode in CONFIG slot ITEMS."
+  (mapcar
+   (lambda (item)
+     (list item
+           (vector
+            (slot-value item 'name)
+            (slot-value item 'cwd)
+            (slot-value item 'cmd))))
+   (slot-value config 'items)))
+
+(defun antics-select-item ()
+  "View an ITEM."
+  (interactive)
+  (start (tabulated-list-get-id))
+  (switch-to-buffer (procname (tabulated-list-get-id))))
+
+(defun antics-refresh ()
+  "Refresh antics content."
+  (interactive)
+  (let* ((config-file-contents (antics--read-config antics-filename))
+         (config (antics--parse-config config-file-contents))
+         (columns (antics--mode-cols))
+         (rows (reverse (antics--mode-rows config))))
+    (setq tabulated-list-format columns)
+    (setq tabulated-list-entries rows)
+    (tabulated-list-init-header)
+    (tabulated-list-print t t)))
+
+(defvar antics-mode-map
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap (kbd "RET") 'antics-select-item)
+    (define-key keymap (kbd "g") 'antics-refresh)
+    keymap)
+  "Key map for antics-mode.")
 
 (define-derived-mode antics-mode tabulated-list-mode "antics"
   "Antics mode"
-  
-  )
+  (antics-refresh))
 
 (defun antics ()
   "Start antics, viewing a list of processes."
@@ -65,8 +141,13 @@
   (switch-to-buffer "*antics*")
   (antics-mode))
 
-(let ((example (antics--read-config "example.antics")))
-  (message "%s" example))
+;; (let ((config (antics--read-config antics-filename)))
+;;   (antics--parse-config config))
+
+;; (let ((example (antics--parse-config (antics--read-config antics-filename))))
+;;   ;; (pp (antics--mode-rows example))
+;;   (with-slots (items) example
+;;     (pp items)))
 
 (provide 'antics)
 ;;; antics.el ends here
